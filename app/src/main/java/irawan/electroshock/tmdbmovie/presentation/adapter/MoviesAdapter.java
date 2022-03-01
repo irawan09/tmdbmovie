40b20d6b5060e9b9486934e75d89560fb68242f0
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-03-01 11:58:37 +0100

    Movies Adapter Added

---
+++ app/src/main/java/irawan/electroshock/tmdbmovie/presentation/adapter/MoviesAdapter.java
@@ -0,0 +1,61 @@
+package irawan.electroshock.tmdbmovie.presentation.adapter;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestBuilder;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
+import java.util.List;
+
+import irawan.electroshock.tmdbmovie.R;
+import irawan.electroshock.tmdbmovie.data.model.MovieEntity;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MovieViewHolder> {
+
+    List<MovieEntity> movieList;
+    private Context context;
+
+    public MoviesAdapter(List<MovieEntity> movieList, Context context) {
+        this.movieList = movieList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_card, parent, false);
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+        holder.movieTitle.setText(movieList.get(position).getTitle());
+
+        RequestBuilder<Drawable> requestBuilder = Glide.with(holder.itemView.getContext())
+                .asDrawable().sizeMultiplier(0.5f);
+
+        Glide.with(context)
+                .load(movieList.get(position).getPosterPath())
+                .thumbnail(requestBuilder)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .placeholder(R.color.cardview_dark_background)
+                .into(holder.movieImage);
+
+        holder.movieDescription.setText(movieList.get(position).getOverview());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieList.size();
+    }
+
+}
--- app/src/main/java/irawan/electroshock/tmdbmovie/presentation/fragment/MoviesFragment.java
+++ app/src/main/java/irawan/electroshock/tmdbmovie/presentation/fragment/MoviesFragment.java
@@ -7,12 +7,15 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import irawan.electroshock.tmdbmovie.R;
+import irawan.electroshock.tmdbmovie.presentation.adapter.MoviesAdapter;
 
 public class MoviesFragment extends Fragment {
 
@@ -27,6 +30,11 @@ public class MoviesFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.movies_fragment, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.recycle_movies);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+//        recyclerView.setAdapter(new MoviesAdapter(null,getContext()));
 
 
         return view;
