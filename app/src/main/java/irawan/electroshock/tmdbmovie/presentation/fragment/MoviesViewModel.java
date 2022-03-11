0b662ccd79f7d4533e9bed8492ce0391b93c7e8a
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-03-11 15:22:27 +0100

    View Binding Update

--- app/src/main/java/irawan/electroshock/tmdbmovie/presentation/fragment/MoviesViewModel.java
+++ app/src/main/java/irawan/electroshock/tmdbmovie/presentation/fragment/MoviesViewModel.java
@@ -1,22 +1,14 @@
 package irawan.electroshock.tmdbmovie.presentation.fragment;
 
-import android.content.Context;
-import android.util.Log;
-
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import javax.inject.Inject;
 
-import irawan.electroshock.tmdbmovie.data.database.Executor;
-import irawan.electroshock.tmdbmovie.data.database.retention.ApplicationContext;
-import irawan.electroshock.tmdbmovie.data.model.MovieEntity;
 import irawan.electroshock.tmdbmovie.data.model.Movies;
-import irawan.electroshock.tmdbmovie.di.module.AppModule;
 import irawan.electroshock.tmdbmovie.di.module.MoviesRepositoryModule;
 
 public class MoviesViewModel extends ViewModel {
@@ -26,8 +18,6 @@ public class MoviesViewModel extends ViewModel {
     @Inject
     public MoviesViewModel(){
     }
-
-    private LiveData<Movies> remoteData;
 
     @Inject
     MoviesRepositoryModule repository;
