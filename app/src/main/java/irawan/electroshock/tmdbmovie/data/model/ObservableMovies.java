0d777db4357862b38e848d828e2a53064ac96444
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-03-18 09:36:21 +0100

    work on observeable reactive java

---
+++ app/src/main/java/irawan/electroshock/tmdbmovie/data/model/ObservableMovies.java
@@ -0,0 +1,84 @@
+package irawan.electroshock.tmdbmovie.data.model;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+@Entity(tableName = "tb_movies")
+public class ObservableMovies implements Serializable {
+
+    @SerializedName("id")
+    @Expose
+    @PrimaryKey()
+    @ColumnInfo(name = "id")
+    private @NonNull String id = "";
+
+    @SerializedName("overview")
+    @Expose
+    @ColumnInfo(name = "overview")
+    private String overview;
+
+    @SerializedName("poster_path")
+    @Expose
+    @ColumnInfo(name = "poster_path")
+    private String posterPath;
+
+    @SerializedName("release_date")
+    @Expose
+    @ColumnInfo(name = "release_date")
+    private String releaseDate;
+
+    @SerializedName("title")
+    @Expose
+    @ColumnInfo(name = "title")
+    private String title;
+
+    @NonNull
+    public String getId() {
+        return id;
+    }
+
+    public void setId(@NonNull String id) {
+        this.id = id;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+}
