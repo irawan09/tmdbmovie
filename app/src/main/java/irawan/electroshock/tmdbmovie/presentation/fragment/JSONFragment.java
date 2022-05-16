42af530655a62dc37c0662d76eee46b0d3cb547c
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-05-16 12:41:59 +0200

    Finish JSON RAW data presentation

--- app/src/main/java/irawan/electroshock/tmdbmovie/presentation/fragment/JSONFragment.java
+++ app/src/main/java/irawan/electroshock/tmdbmovie/presentation/fragment/JSONFragment.java
@@ -2,7 +2,6 @@ package irawan.electroshock.tmdbmovie.presentation.fragment;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,14 +10,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import irawan.electroshock.tmdbmovie.R;
 import irawan.electroshock.tmdbmovie.databinding.JsonFragmentBinding;
 
 public class JSONFragment extends Fragment {
 
-    private static final String TAG = "JSONFragment data";
     private JsonFragmentBinding jsonFragmentBinding;
-
 
     public static JSONFragment newInstance() {
         return new JSONFragment();
