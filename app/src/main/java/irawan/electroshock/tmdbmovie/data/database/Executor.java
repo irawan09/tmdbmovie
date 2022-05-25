43aa7fddd6778028060880dd248e4c061e615ba7
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-05-25 12:40:12 +0200

    Concurrency works good

--- app/src/main/java/irawan/electroshock/tmdbmovie/data/database/Executor.java
+++ app/src/main/java/irawan/electroshock/tmdbmovie/data/database/Executor.java
@@ -1,20 +1,11 @@
 package irawan.electroshock.tmdbmovie.data.database;
 
-import static java.util.concurrent.Executors.newFixedThreadPool;
-
 import android.util.Log;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-
-import irawan.electroshock.tmdbmovie.data.database.dao.MoviesDao;
-import irawan.electroshock.tmdbmovie.data.model.Movies;
 
 public class Executor {
     private static String TAG = "Executors";
