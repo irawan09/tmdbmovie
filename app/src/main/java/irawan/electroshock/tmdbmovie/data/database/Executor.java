fa31ac62420463a31047b5ff9d1802a6a10c2ddf
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-05-26 19:52:08 +0200

    Update

--- app/src/main/java/irawan/electroshock/tmdbmovie/data/database/Executor.java
+++ app/src/main/java/irawan/electroshock/tmdbmovie/data/database/Executor.java
@@ -5,7 +5,6 @@ import android.util.Log;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 public class Executor {
     private static final String TAG = "Executors";
